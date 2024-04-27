8c37b9b3ab75d48656cf801987a6bcaa90bcc089
bobdylan <894751140@qq.com>
2024-04-27 12:30:37 +0800

    JPS begin

---
+++ AStarPlaning/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/rob/RobitcsPathPlanning/AStarPlaning/src
+# Build directory: /home/rob/RobitcsPathPlanning/AStarPlaning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("grid_path_searcher")
+subdirs("rviz_plugins")
+subdirs("waypoint_generator")
